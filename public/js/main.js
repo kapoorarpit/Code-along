211092aadc3d243309a69d51985ebc425cf3b3e7
kapoorarpit <kapoorarpit2000@gmail.com>
2021-05-10 21:15:23 +0530

    msg_display

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,10 @@
 const chatForm = document.getElementById('chat-form');
 const socket = io();
 
+// message from server
 socket.on('message', message=>{
     console.log(message);
+    outputmessage(message);
 });
 
 // message submit 
@@ -14,4 +16,17 @@ chatForm.addEventListener('submit',(e)=>{
 
     // emit a message to server
     socket.emit('chatmessage',msg);
+
+    // output msg to dom
+
 })
+
+function outputmessage(message){
+    const div= document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML=  `<p class="meta">Brad <span>9:12pm</span></p>
+                    <p class="text">
+                    ${message}
+                    </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
